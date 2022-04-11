7de6a133477782bd005567350d3cf6d8e3ef8750
Dick Swart <dick.swart@me.com>
2022-04-12 07:09:56 +1200

    updated custom components

--- custom_components/echonetlite/__init__.py
+++ custom_components/echonetlite/__init__.py
@@ -2,17 +2,25 @@
 from __future__ import annotations
 import logging
 import pychonet as echonet
-from pychonet.lib.epc import EPC_SUPER
+from pychonet.lib.epc import EPC_SUPER, EPC_CODE
+from pychonet.lib.const import VERSION
 from datetime import timedelta
 import asyncio
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.util import Throttle
-from .const import DOMAIN
+from .const import DOMAIN, USER_OPTIONS, TEMP_OPTIONS
 from aioudp import UDPServer
-# from pychonet import Factory
+
 from pychonet import ECHONETAPIClient
-from pychonet.EchonetInstance import ENL_GETMAP, ENL_SETMAP, ENL_UID, ENL_STATUS, ENL_CUMULATIVE_POWER
+from pychonet.EchonetInstance import (
+    ENL_GETMAP,
+    ENL_SETMAP,
+    ENL_UID,
+    ENL_STATUS,
+    ENL_INSTANTANEOUS_POWER,
+    ENL_CUMULATIVE_POWER
+)
 from pychonet.HomeAirConditioner import (
     ENL_FANSPEED,
     ENL_AUTO_DIRECTION,
@@ -21,21 +29,34 @@ from pychonet.HomeAirConditioner import (
     ENL_AIR_HORZ,
     ENL_HVAC_MODE,
     ENL_HVAC_SET_TEMP,
+    ENL_HVAC_ROOM_HUMIDITY,
     ENL_HVAC_ROOM_TEMP,
     ENL_HVAC_OUT_TEMP
 )
 
+from pychonet.GeneralLighting import (
+    ENL_BRIGHTNESS,
+    ENL_COLOR_TEMP
+)
+
 _LOGGER = logging.getLogger(__name__)
-PLATFORMS = ["sensor", 'climate', 'select', 'light']
+PLATFORMS = ["sensor", 'climate', 'select', 'light', 'fan']
 PARALLEL_UPDATES = 0
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
 MAX_UPDATE_BATCH_SIZE = 10
 
 HVAC_API_CONNECTOR_DEFAULT_FLAGS = [
     ENL_STATUS, ENL_FANSPEED, ENL_AUTO_DIRECTION, ENL_SWING_MODE, ENL_AIR_VERT, ENL_AIR_HORZ, ENL_HVAC_MODE,
-    ENL_HVAC_SET_TEMP, ENL_HVAC_ROOM_TEMP, ENL_HVAC_OUT_TEMP, ENL_CUMULATIVE_POWER
+    ENL_HVAC_SET_TEMP, ENL_HVAC_ROOM_HUMIDITY, ENL_HVAC_ROOM_TEMP, ENL_HVAC_OUT_TEMP,
+    ENL_INSTANTANEOUS_POWER, ENL_CUMULATIVE_POWER
 ]
 
+LIGHT_API_CONNECTOR_DEFAULT_FLAGS = [
+    ENL_STATUS, ENL_BRIGHTNESS, ENL_COLOR_TEMP
+]
+
+# fix later
+_0287_API_CONNECTOR_DEFAULT_FLAGS = [ENL_STATUS, 0xC0, 0xC1, 0xC2, 0xC5, 0xC6, 0xC7, 0xC8]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     entry.async_on_unload(entry.add_update_listener(update_listener))
@@ -44,8 +65,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     loop = None
     server = None
 
+    _LOGGER.debug(f"pychonet version {VERSION}")
     if DOMAIN in hass.data:  # maybe set up by config entry?
-        _LOGGER.debug(f"{hass.data[DOMAIN]} has already been setup..")
+        _LOGGER.debug(f"{hass.data[DOMAIN]} is already running.")
         server = hass.data[DOMAIN]['api']
         hass.data[DOMAIN].update({entry.entry_id: []})
     else:  # setup API
@@ -55,7 +77,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         loop = asyncio.get_event_loop()
         udp.run("0.0.0.0", 3610, loop=loop)
         server = ECHONETAPIClient(server=udp, loop=loop)
+        server._message_timeout = 300
         hass.data[DOMAIN].update({"api": server})
+
 
     for instance in entry.data["instances"]:
         echonetlite = None
@@ -66,7 +90,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         getmap = instance["getmap"]
         setmap = instance["setmap"]
         uid = instance["uid"]
-        _LOGGER.debug(f'{instance["uid"]} is the UID..')
 
         # manually update API states using config entry data
         if host not in list(server._state):
@@ -133,40 +156,19 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return unload_ok
 
 
-# TODO FIX CODE REPETITION
+# TODO update for Air Cleaner
 async def update_listener(hass, entry):
     for instance in hass.data[DOMAIN][entry.entry_id]:
         if instance['instance']['eojgc'] == 1 and instance['instance']['eojcc'] == 48:
-            if entry.options.get("fan_settings") is not None:  # check if options has been created
-                if len(entry.options.get("fan_settings")) > 0:  # if it has been created then check list length.
-                    instance["echonetlite"]._user_options.update({ENL_FANSPEED: entry.options.get("fan_settings")})
-                else:
-                    instance["echonetlite"]._user_options.update({ENL_FANSPEED: False})
-
-            if entry.options.get("swing_horiz") is not None:
-                if len(entry.options.get("swing_horiz")) > 0:
-                    instance["echonetlite"]._user_options.update({ENL_AIR_HORZ: entry.options.get("swing_horiz")})
-                else:
-                    instance["echonetlite"]._user_options.update({ENL_AIR_HORZ: False})
-
-            if entry.options.get("swing_vert") is not None:
-                if len(entry.options.get("swing_vert")) > 0:
-                    instance["echonetlite"]._user_options.update({ENL_AIR_VERT: entry.options.get("swing_vert")})
-                else:
-                    instance["echonetlite"]._user_options.update({ENL_AIR_VERT: False})
-
-            if entry.options.get("auto_direction") is not None:
-                if len(entry.options.get("auto_direction")) > 0:
-                    instance["echonetlite"]._user_options.update({ENL_AUTO_DIRECTION: entry.options.get("auto_direction")})
-                else:
-                    instance["echonetlite"]._user_options.update({ENL_AUTO_DIRECTION: False})
-
-            if entry.options.get("swing_mode") is not None:
-                if len(entry.options.get("swing_mode")) > 0:
-                    instance["echonetlite"]._user_options.update({ENL_SWING_MODE: entry.options.get("swing_mode")})
-                else:
-                    instance["echonetlite"]._user_options.update({ENL_SWING_MODE: False})
-
+            for option in USER_OPTIONS.keys():
+                if entry.options.get(USER_OPTIONS[option]["option"]) is not None:  # check if options has been created
+                    if len(entry.options.get(USER_OPTIONS[option]["option"])) > 0:  # if it has been created then check list length.
+                        instance["echonetlite"]._user_options.update({option: entry.options.get(USER_OPTIONS[option]["option"])})
+                    else:
+                        instance["echonetlite"]._user_options.update({option: False})
+            for option in TEMP_OPTIONS.keys():
+                if entry.options.get(option) is not None:
+                        instance["echonetlite"]._user_options.update({option: entry.options.get(option)})
 
 class ECHONETConnector():
     """EchonetAPIConnector is used to centralise API calls for  Echonet devices.
@@ -188,17 +190,29 @@ class ECHONETConnector():
 
         # Detect HVAC - eventually we will use factory here.
         self._update_flags_full_list = []
-        if self._eojgc == 1 and self._eojcc == 48:
-            for value in HVAC_API_CONNECTOR_DEFAULT_FLAGS:
-                if value in self._getPropertyMap:
-                    self._update_flags_full_list.append(value)
-            self._instance = echonet.HomeAirConditioner(self._host, self._api)
+        flags = []
+        if self._eojgc == 0x01 and self._eojcc == 0x30:
+            _LOGGER.debug(f"Starting ECHONETLite HomeAirConditioner instance at {self._host}")
+            flags = HVAC_API_CONNECTOR_DEFAULT_FLAGS
+        elif self._eojgc == 0x02 and self._eojcc == 0x90:
+            _LOGGER.debug(f"Starting ECHONETLite GeneralLighting instance at {self._host}")
+            flags = LIGHT_API_CONNECTOR_DEFAULT_FLAGS
+        elif self._eojgc == 0x02 and self._eojcc == 0x87:
+            _LOGGER.debug(f"Starting ECHONETLite DistributionPanelMeter instance at {self._host}")
+            flags = _0287_API_CONNECTOR_DEFAULT_FLAGS
         else:
-            self._update_flags_full_list = [ENL_STATUS]
+            _LOGGER.debug(f"Starting ECHONETLite Generic instance for {self._eojgc}-{self._eojcc}-{self._eojci} at {self._host}")
+            flags = [ENL_STATUS]
             for item in self._getPropertyMap:
                 if item not in list(EPC_SUPER.keys()):
-                    self._update_flags_full_list.append(item)
-            self._instance = echonet.Factory(self._host, self._api, self._eojgc, self._eojcc, self._eojci)
+                    if item in list(EPC_CODE[self._eojgc][self._eojcc].keys()):
+                        flags.append(item)
+
+        for value in flags:
+            if value in self._getPropertyMap:
+                self._update_flags_full_list.append(value)
+                self._update_data[value] = None
+        self._instance = echonet.Factory(self._host, self._api, self._eojgc, self._eojcc, self._eojci)
 
         # Split list of codes into batches of 10
         start_index = 0
@@ -209,33 +223,33 @@ class ECHONETConnector():
             start_index += MAX_UPDATE_BATCH_SIZE
         self._update_flag_batches.append(self._update_flags_full_list[start_index:full_list_length])
 
+        # TODO this looks messy.
         self._user_options = {
             ENL_FANSPEED: False,
             ENL_AUTO_DIRECTION: False,
             ENL_SWING_MODE: False,
             ENL_AIR_VERT: False,
-            ENL_AIR_HORZ: False
+            ENL_AIR_HORZ: False,
+            'min_temp_heat': 15,
+            'max_temp_heat': 35,
+            'min_temp_cool': 15,
+            'max_temp_cool': 35,
+            'min_temp_auto': 15,
+            'max_temp_auto': 35,
         }
-        # Stitch together user selectable options for fan + swing modes for HVAC
-        # TODO - fix code repetition
-        if entry.options.get("fan_settings") is not None:  # check if options has been created
-            if len(entry.options.get("fan_settings")) > 0:  # if it has been created then check list length.
-                self._user_options[ENL_FANSPEED] = entry.options.get("fan_settings")
-        if entry.options.get("swing_horiz") is not None:
-            if len(entry.options.get("swing_horiz")) > 0:
-                self._user_options[ENL_AIR_HORZ] = entry.options.get("swing_horiz")
-        if entry.options.get("swing_vert") is not None:  # check if options has been created
-            if len(entry.options.get("swing_vert")) > 0:
-                self._user_options[ENL_AIR_VERT] = entry.options.get("swing_vert")
-        if entry.options.get("auto_direction") is not None:  # check if options has been created
-            if len(entry.options.get("auto_direction")) > 0:
-                self._user_options[ENL_AUTO_DIRECTION] = entry.options.get("auto_direction")
-        if entry.options.get("swing_mode") is not None:  # check if options has been created
-            if len(entry.options.get("swing_mode")) > 0:
-                self._user_options[ENL_SWING_MODE] = entry.options.get("swing_mode")
+        # User selectable options for fan + swing modes for HVAC
+        for option in USER_OPTIONS.keys():
+            if entry.options.get(USER_OPTIONS[option]['option']) is not None:  # check if options has been created
+                if len(entry.options.get(USER_OPTIONS[option]['option'])) > 0:  # if it has been created then check list length.
+                    self._user_options[option] = entry.options.get(USER_OPTIONS[option]['option'])
+
+        # Temperature range options for heat, cool and auto modes
+        for option in TEMP_OPTIONS.keys():
+            if entry.options.get(option) is not None:
+                self._user_options[option] = entry.options.get(option)
 
         self._uid = self._api._state[self._host]["instances"][self._eojgc][self._eojcc][self._eojci][ENL_UID]
-        _LOGGER.debug(f'{self._uid} is the UID in ECHONET connector..')
+        _LOGGER.debug(f'ECHONET instance UID is {self._uid}')
         if self._uid is None:
             self._uid = f"{self._host}-{self._eojgc}-{self._eojcc}-{self._eojci}"
 
@@ -250,14 +264,14 @@ class ECHONETConnector():
                         update_data.update(batch_data)
                     elif len(flags) == 1:
                         update_data[flags[0]] = batch_data
-            _LOGGER.debug(f"{list(update_data.values())}")
+            _LOGGER.debug(f"ECHONETlite polling update data - {list(update_data.values())}")
             if len(update_data) > 0 and False not in list(update_data.values()):
                 # polling succeded.
                 if retry > 1:
-                    _LOGGER.debug(f"polling ECHONET Instance host {self._host} succeeded - Retry {retry} of 3")
-                self._update_data = update_data
+                    _LOGGER.debug(f"Polling ECHONET Instance host {self._host} succeeded. Retry {retry} of 3")
+                self._update_data.update(update_data)
                 return self._update_data
             else:
-                _LOGGER.debug(f"polling ECHONET Instance host {self._host} timed out - Retry {retry} of 3")
-                _LOGGER.debug(f"Number of missed ECHONETLite msssages since reboot is - {len(self._api._message_list)}")
+                _LOGGER.debug(f"Polling ECHONET Instance host {self._host} timed out. Retry {retry} of 3")
+                _LOGGER.debug(f"Number of missed ECHONETLite msssages since reboot is {len(self._api._message_list)}")
         return self._update_data
--- custom_components/echonetlite/sensor.py
+++ custom_components/echonetlite/sensor.py
@@ -2,8 +2,8 @@
 import logging
 
 from homeassistant.const import (
-    CONF_ICON, CONF_TYPE,
-    DEVICE_CLASS_TEMPERATURE, DEVICE_CLASS_ENERGY,
+    CONF_ICON, CONF_TYPE, DEVICE_CLASS_HUMIDITY, DEVICE_CLASS_POWER,
+    DEVICE_CLASS_TEMPERATURE, DEVICE_CLASS_ENERGY, PERCENTAGE, POWER_WATT,
     TEMP_CELSIUS, ENERGY_WATT_HOUR,
     STATE_UNAVAILABLE
 )
@@ -11,6 +11,7 @@ from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.typing import StateType
 
 from pychonet.lib.epc import EPC_CODE, EPC_SUPER
+from pychonet.lib.eojx import EOJX_CLASS
 from .const import (
     DOMAIN,
     ENL_SENSOR_OP_CODES,
@@ -23,12 +24,26 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass, config, async_add_entities, discovery_info=None):
     entities = []
     for entity in hass.data[DOMAIN][config.entry_id]:
-        _LOGGER.debug(entity)
+        _LOGGER.debug(f"Configuring ECHONETLite sensor {entity}")
+        _LOGGER.debug(f"Update flags for this sensor are {entity['echonetlite']._update_flags_full_list}")
         eojgc = entity['instance']['eojgc']
         eojcc = entity['instance']['eojcc']
 
         # Home Air Conditioner we dont bother exposing all sensors
         if eojgc == 1 and eojcc == 48:
+            _LOGGER.debug("This is an ECHONET climate device so not all sensors will be configured.")
+            for op_code in ENL_SENSOR_OP_CODES[eojgc][eojcc].keys():
+                if op_code in entity['instance']['getmap']:
+                    entities.append(
+                        EchonetSensor(
+                            entity['echonetlite'],
+                            op_code,
+                            ENL_SENSOR_OP_CODES[eojgc][eojcc][op_code],
+                            config.title
+                        )
+                    )
+        elif eojgc == 1 and eojcc == 53:
+            _LOGGER.debug("This is an ECHONET fan device so not all sensors will be configured.")
             for op_code in ENL_SENSOR_OP_CODES[eojgc][eojcc].keys():
                 if op_code in entity['instance']['getmap']:
                     entities.append(
@@ -56,6 +71,10 @@ async def async_setup_entry(hass, config, async_add_entities, discovery_info=Non
                             entities.append(
                                 EchonetSensor(entity['echonetlite'], op_code, ENL_SENSOR_OP_CODES['default'], config.title)
                             )
+                    elif op_code in list(entity['echonetlite']._update_flags_full_list):
+                        entities.append(
+                            EchonetSensor(entity['echonetlite'], op_code, ENL_SENSOR_OP_CODES['default'], config.title)
+                        )
                 elif op_code in list(entity['echonetlite']._update_flags_full_list):
                     entities.append(
                         EchonetSensor(entity['echonetlite'], op_code, ENL_SENSOR_OP_CODES['default'], config.title)
@@ -89,6 +108,12 @@ class EchonetSensor(SensorEntity):
             self._unit_of_measurement = TEMP_CELSIUS
         elif self._sensor_attributes[CONF_TYPE] == DEVICE_CLASS_ENERGY:
             self._unit_of_measurement = ENERGY_WATT_HOUR
+        elif self._sensor_attributes[CONF_TYPE] == DEVICE_CLASS_POWER:
+            self._unit_of_measurement = POWER_WATT
+        elif self._sensor_attributes[CONF_TYPE] == DEVICE_CLASS_HUMIDITY:
+            self._unit_of_measurement = PERCENTAGE
+        elif self._sensor_attributes[CONF_TYPE] == PERCENTAGE:
+            self._unit_of_measurement = PERCENTAGE
         else:
             self._unit_of_measurement = None
 
@@ -114,30 +139,49 @@ class EchonetSensor(SensorEntity):
                 (DOMAIN, self._instance._uid, self._instance._eojgc, self._instance._eojcc, self._instance._eojci)
             },
             "name": self._device_name,
-            "manufacturer": self._instance._manufacturer
-            # "model": "",
+            "manufacturer": self._instance._manufacturer,
+            "model": EOJX_CLASS[self._eojgc][self._eojcc]
             # "sw_version": "",
         }
 
     @property
     def native_value(self) -> StateType:
         """Return the state of the sensor."""
-        if self._sensor_attributes[CONF_TYPE] == DEVICE_CLASS_TEMPERATURE:
-            if self._op_code in self._instance._update_data:
-                if self._instance._update_data[self._op_code] == 126 or self._instance._update_data[self._op_code] is None:
-                    return STATE_UNAVAILABLE
+        if self._op_code in self._instance._update_data:
+            if self._op_code == 0xC0 or self._op_code == 0xC1: # kludge for distribution panel meter.
+               if self._eojgc == 0x02 and self._eojcc == 0x87 and 0xC2 in self._instance._update_data:
+                   if self._instance._update_data[0xC2] is not None and self._instance._update_data[self._op_code] is not None:
+                        return self._instance._update_data[self._op_code] * self._instance._update_data[0xC2] * 1000 # value in Wh
+
+            if self._instance._update_data[self._op_code] is None:
+                return STATE_UNAVAILABLE
+            elif self._sensor_attributes[CONF_TYPE] in [
+                    DEVICE_CLASS_TEMPERATURE, DEVICE_CLASS_HUMIDITY
+            ]:
+                if self._op_code in self._instance._update_data:
+                    if self._instance._update_data[self._op_code] in [126, 253]:
+                        return STATE_UNAVAILABLE
+                    else:
+                        return self._instance._update_data[self._op_code]
                 else:
+                    return STATE_UNAVAILABLE
+            elif self._sensor_attributes[CONF_TYPE] == DEVICE_CLASS_POWER:
+                if self._op_code in self._instance._update_data:
+                    # Underflow (less than 1 W)
+                    if self._instance._update_data[self._op_code] == 65534:
+                        return 1
+                    else:
+                        return self._instance._update_data[self._op_code]
+                else:
+                    return STATE_UNAVAILABLE
+            elif self._op_code in self._instance._update_data:
+                if isinstance(self._instance._update_data[self._op_code], (int, float)):
                     return self._instance._update_data[self._op_code]
-            else:
-                return STATE_UNAVAILABLE
-        elif self._op_code in self._instance._update_data:
-            if isinstance(self._instance._update_data[self._op_code], (int, float)):
-                return self._instance._update_data[self._op_code]
-            if len(self._instance._update_data[self._op_code]) < 255:
-                return self._instance._update_data[self._op_code]
-            else:
-                return STATE_UNAVAILABLE
-        return None
+                if len(self._instance._update_data[self._op_code]) < 255:
+                    return self._instance._update_data[self._op_code]
+                else:
+                    return STATE_UNAVAILABLE
+        return STATE_UNAVAILABLE
 
     @property
     def native_unit_of_measurement(self):
